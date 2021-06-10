fa9c74172b7c52fb32f1f10752e74087f081023c
waihinchan <52313420+waihinchan@users.noreply.github.com>
2021-06-10 12:05:49 +0800

    Init

---
+++ Assets/script/init_scene.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init_scene : MonoBehaviour
+{   public SceneInitClass sceneinit; //这个应该是一个实例化对象
+    private List<GameObject> chess_list;
+    // Start is called before the first frame update
+    void Start()
+    {      
+        // get the size to put it one by one
+        Renderer rend;
+        GameObject chess_board = sceneinit.chess_unit.transform.GetChild(1).gameObject;
+        rend = chess_board.GetComponent<Renderer>();
+        Vector3 unit_size = rend.bounds.size;
+        int row = (int)Mathf.Sqrt(sceneinit.checkerboards);
+        int col = row;
+        Vector3 unit_position;
+        // get the size to put it one by one
+        chess_list = new List<GameObject>();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                Vector3 current_index = new Vector3((float)i*sceneinit.interval,0,(float)j*sceneinit.interval);
+                unit_position = Vector3.Scale(unit_size , current_index); 
+                // Debug.Log(unit_position);
+                GameObject chess = Instantiate(sceneinit.chess_unit);
+                chess.transform.position = unit_position;
+                chess_list.Add(chess);
+                //这里还要顾及到一个问题是相机的定位问题 底座可以随便下载一个素材搞的漂亮一点的
+            }      
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
+

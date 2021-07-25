2ba37759dd0cc74b676941908bb91144ae0d5854
illidanli <530195725@qq.com>
2021-07-25 22:07:28 +0800

    fc

--- Assets/Main/Scripts/Logic/ChessBoard.cs
+++ Assets/Main/Scripts/Logic/ChessBoard.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class ChessBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
     public const int ChessWidth = 6; //最大棋盘数
-    
-    public GameObject chessBass; //这个是棋盘的底座
+
+    // Start is called before the first frame update
+    public int totalChessBass = 36; //最大棋盘数..
+    public GameObject chessBass; //这个是棋盘的底座.
     public Transform offset;
     public Chess[][] ChessMatrix;
 
@@ -16,10 +17,33 @@ public class ChessBoard : MonoBehaviour
     
     void Start()
     {
-        userManager = GameObject.Find("Manager").GetComponent<UserManager>();
+        // userManager = GameObject.Find("Manager").GetComponent<UserManager>();
         InitChessBoard();
+        PlaceChessBases(); //这里是游戏开始才初始化棋盘还是说直接进入这个场景就可以初始化棋盘还不确定
     }
 
+    void PlaceChessBases(){
+
+        if(offset==null){
+            offset = transform;
+        }
+        float x = chessBass.GetComponent<MeshFilter>().sharedMesh.bounds.size.x * chessBass.transform.lossyScale.x;
+        float y = chessBass.GetComponent<MeshFilter>().sharedMesh.bounds.size.y * chessBass.transform.lossyScale.y;
+        float z = chessBass.GetComponent<MeshFilter>().sharedMesh.bounds.size.z * chessBass.transform.lossyScale.z;
+        Vector3 singlesize = new Vector3(x,y,z);
+        int rows = (int)Mathf.Floor(Mathf.Sqrt(totalChessBass));
+        int cols = rows;
+        int count = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                GameObject singlechess = Instantiate(chessBass, new Vector3(i*singlesize.x + 0.1f * i ,0,j*singlesize.z + j * 0.1f) + offset.position, Quaternion.identity);
+                count++;
+            }
+        }
+    }
+    
     void InitChessBoard()
     {
         ChessMatrix = new Chess[ChessWidth][];
@@ -54,7 +78,7 @@ public class ChessBoard : MonoBehaviour
         
     }
     
-    public void RemoveChess(Tuple<int, int> index)
+    public void RemoveChess(ValueTuple<int, int> index)
     {
         if (ChessMatrix[index.Item1][index.Item2] != null)
         {
